29361d6a578e63503c08dfc25023dac7f5de7da5
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-07-17 13:42:38 +0530

    Expose interactive visualisation API

--- src/dataviz_reference/__init__.py
+++ src/dataviz_reference/__init__.py
@@ -6,14 +6,22 @@ from .charts import (
     ranked_bar_chart,
     time_series_chart,
 )
+from .interactive import (
+    interactive_choropleth,
+    interactive_scatter,
+    interactive_time_series,
+)
 from .theme import apply_accessible_theme
 
 __all__ = [
     "apply_accessible_theme",
     "correlation_heatmap",
     "distribution_plot",
+    "interactive_choropleth",
+    "interactive_scatter",
+    "interactive_time_series",
     "ranked_bar_chart",
     "time_series_chart",
 ]
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
